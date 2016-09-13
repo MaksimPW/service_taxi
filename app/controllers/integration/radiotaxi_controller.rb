f1238e9f0bead54ef1dac283f4ed88b3273b4514
MaksimPW <ma.savichev@gmail.com>
2016-09-13 19:47:05 +0300

    integration-radiotaxi | Change database connection on default

--- app/controllers/integration/radiotaxi_controller.rb
+++ app/controllers/integration/radiotaxi_controller.rb
@@ -5,15 +5,24 @@ class Integration::RadiotaxiController < ApplicationController
     sql = "SELECT zip, marka, nomer, zip_vod FROM AVTO;"
     cursor = ActiveRecord::Base.connection.execute(sql)
 
+    h = Hash.new
+    i = 0
+
     while row = cursor.fetch(:hash)
       break if row['NAME'] =~ /e 13/
 
-      h = Hash.new
-      h['id'] = row['zip']
-      h['mark'] = row['marka']
-      h['license_number'] = row['nomer']
-      h['driver_id'] = row['zip_vod']
+      h[i]['id'] = row['zip']
+      h[i]['mark'] = row['marka']
+      h[i]['license_number'] = row['nomer']
+      h[i]['driver_id'] = row['zip_vod']
+      i+=1
+    end
 
+    cursor.close
+    ActiveRecord::Base.remove_connection
+    ActiveRecord::Base.establish_connection
+
+    h.each do |h|
       if car = Car.where(id: h['id'])
         car.save!(
             mark: h['mark'],
@@ -29,8 +38,6 @@ class Integration::RadiotaxiController < ApplicationController
         )
       end
     end
-
-    cursor.close
     render nothing: true
   end
 
@@ -38,22 +45,29 @@ class Integration::RadiotaxiController < ApplicationController
     sql = "SELECT zip, fam_long, dop_info FROM PERSONEL;"
     cursor = ActiveRecord::Base.connection.execute(sql)
 
+    h = Hash.new
+    i = 0
+
     while row = cursor.fetch(:hash)
       break if row['NAME'] =~ /e 13/
-
       h = Hash.new
       h['id'] = row['zip']
       h['fio'] = row['fam_long']
       h['description'] = row['dop_info']
+      i+=1
+    end
 
+    cursor.close
+    ActiveRecord::Base.remove_connection
+    ActiveRecord::Base.establish_connection
+
+    h.each do |h|
       if driver = Driver.where(id: h['id'])
         driver.save!(fio: h['fio'], description: h['description'])
       else
         Driver.create!(fio: h['fio'], description: h['description'])
       end
     end
-
-    cursor.close
     render nothing: true
   end
 
