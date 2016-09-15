324bd7020e4fe0468a883fbacd22a1ea12fe14b0
MaksimPW <ma.savichev@gmail.com>
2016-09-15 23:44:18 +0300

    integration-onec | Simple Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    scope module: 'integration' do
-      root 'georitm#init'
-      get 'georitm/ping' => 'georitm#ping'
-      get 'georitm/init' => 'georitm#init'
-      post 'georitm/execute' => 'georitm#execute'
+  devise_for :users
+  scope module: 'integration' do
+    root 'georitm#init'
+    get 'georitm/ping' => 'georitm#ping'
+    get 'georitm/init' => 'georitm#init'
+    post 'georitm/execute' => 'georitm#execute'
 
-      post 'radiotaxi/cars' => 'radiotaxi#cars'
-      post 'radiotaxi/drivers' => 'radiotaxi#drivers'
-      post 'radiotaxi/orders' => 'radiotaxi#orders'
-    end
+    post 'radiotaxi/cars' => 'radiotaxi#cars'
+    post 'radiotaxi/drivers' => 'radiotaxi#drivers'
+    post 'radiotaxi/orders' => 'radiotaxi#orders'
   end
 end
