9c1d61da521156bcb572093df0d711bd86367672
MaksimPW <ma.savichev@gmail.com>
2016-09-21 21:19:30 +0300

    user-settings | rspec features sign_in

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ end
 group :test do
   gem 'shoulda-matchers'
   gem 'fakeweb'
+  gem 'capybara'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   use_doorkeeper
   devise_for :users
+
+  root 'dummy#index'
+
   scope module: 'integration' do
-    root 'georitm#init'
     get 'georitm/ping' => 'georitm#ping'
     get 'georitm/init' => 'georitm#init'
     post 'georitm/execute' => 'georitm#execute'
