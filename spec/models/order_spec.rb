7c2b88f856630bfc3858a68a674f14087b58b2a3
MaksimPW <ma.savichev@gmail.com>
2016-09-22 19:09:32 +0300

    define-geodata | rspec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
+  context '.define_geodata' do
+    it 'should receive' do
+      order = Order.create(begin_address: 'Moscow', end_address: 'St. Petersburg')
+      expect(order).to receive(:define_geodata)
+      order.save
+    end
+  end
 end
