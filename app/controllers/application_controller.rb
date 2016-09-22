d377e2a35a8f8230aa2d8c135b4a7d7b48171c9b
MaksimPW <ma.savichev@gmail.com>
2016-09-22 16:47:51 +0300

    user-settings | include Devise::Test::ControllerHelpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :set_paper_trail_whodunnit
 
   # Prevent CSRF attacks by raising an exception.
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,6 +57,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Capybara::DSL
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
 
 Shoulda::Matchers.configure do |config|
