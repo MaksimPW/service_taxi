c0eacbf8d6915d6e531dc58d5fcac4b8e412aa8f
MaksimPW <ma.savichev@gmail.com>
2016-09-26 20:24:50 +0300

    inspection-1 | geocode for place address

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :place_type
+
+  geocoded_by :address, :latitude  => :lat, :longitude => :lon
+  after_save :geocode, :unless => :address_empty?
+
+  def address_empty?
+    address.empty?
+  end
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '.geocode' do
+    let(:subject) { FactoryGirl.create(:place, address: 'St. Petersburg', lon: nil, lat: nil) }
+
+    it 'should receive if address' do
+      expect(subject).to receive(:geocode)
+      subject.save!
+    end
+
+    it 'should not receive if address empty' do
+      subject.address = ''
+      expect(subject).to_not receive(:geocode)
+      subject.save!
+    end
+  end
 end
