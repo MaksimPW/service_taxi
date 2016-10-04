f5bc9ee3616053789f25b084bbd88db7ac1fea0b
MaksimPW <ma.savichev@gmail.com>
2016-10-05 00:39:58 +0300

    inspection-1 | distance_between units: :km

--- app/models/status_car.rb
+++ app/models/status_car.rb
@@ -4,7 +4,7 @@ class StatusCar < ActiveRecord::Base
     places = Place.all
     @location_types = Array.new
     places.each do |p|
-      if Geocoder::Calculations.distance_between([self.geo_lat, self.geo_lon],[p.lat, p.lon]) < p.radius
+      if Geocoder::Calculations.distance_between([self.geo_lat, self.geo_lon],[p.lat, p.lon], units: :km) < p.radius
          @location_types << p.place_type_id
       end
     end
--- spec/models/status_car_spec.rb
+++ spec/models/status_car_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe StatusCar, type: :model do
   context '#get_places' do
 
     let(:place_type1) { FactoryGirl.create(:place_type, id: 1) }
-    let(:place_type2) { FactoryGirl.create(:place_type, id: 1) }
-    let(:in_place) { FactoryGirl.create(:place, lat: 59.9342802, lon: 30.3350986, radius: 1, place_type_id: place_type1.id) }
-    let(:out_place) { FactoryGirl.create(:place, lat: 60.04084627, lon: 30.02872467, radius: 1, place_type_id: place_type2.id) }
+    let(:place_type2) { FactoryGirl.create(:place_type, id: 2) }
+    let!(:in_place) { FactoryGirl.create(:place, lat: 59.9342802, lon: 30.3350986, radius: 1, place_type_id: place_type1.id) }
+    let!(:out_place) { FactoryGirl.create(:place, lat: 60.0408462, lon: 30.02872467, radius: 1, place_type_id: place_type2.id) }
     let!(:status) { FactoryGirl.create(:status_car, geo_lat: 59.93402709, geo_lon: 30.33640087) }
+    let!(:status_distance_between_km_and_mile) { FactoryGirl.create(:status_car, geo_lat: 59.942297, geo_lon: 30.355756) }
 
     it 'should receive' do
       expect(status).to receive(:get_places)
@@ -17,6 +18,11 @@ RSpec.describe StatusCar, type: :model do
     it 'return array' do
       statuses = [in_place.id]
       expect(status.get_places).to match_array(statuses)
+    end
+
+    it 'units = km' do
+      statuses = []
+      expect(status_distance_between_km_and_mile.get_places).to match_array(statuses)
     end
   end
 end
