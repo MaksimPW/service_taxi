fd6154a462c450e327d5977a289c318725db0040
MaksimPW <ma.savichev@gmail.com>
2016-10-04 06:51:00 +0300

    inspection-1 | Ref

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  context '.define_geodata' do
+  context '#define_geodata' do
     it 'should receive' do
       order = Order.create(begin_address: 'Moscow', end_address: 'St. Petersburg')
       expect(order).to receive(:define_geodata)
@@ -9,7 +9,7 @@ RSpec.describe Order, type: :model do
     end
   end
 
-  context '.define_track' do
+  context '#define_track' do
     let(:car) { FactoryGirl.create(:car) }
     let!(:status1) { FactoryGirl.create(:status_car, car_id: car.id, fixed_time: Time.now - 5.hours) }
     let!(:status2) { FactoryGirl.create(:status_car, car_id: car.id, fixed_time: Time.now - 3.hours) }
