c6acffa2bd2c537c5011eb3743d1e887bac125fb
MaksimPW <ma.savichev@gmail.com>
2016-10-09 09:03:46 +0300

    new-vision-app | #1 Track.define_type for 3 inspection

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -25,35 +25,62 @@ RSpec.describe Track, type: :model do
 
     context 'with order' do
       context 'inspection-4 | Превышен лимит по дистанции' do
-      let!(:order) { create(:order,
-                            take_time: '2016-10-09 12:00:00',
-                            end_time: '2016-10-09 13:00:00',
-                            car_id: car.id,
-                            begin_address: 'МО №17 "Шувалово-Озерки", Питер',
-                            end_address: 'Северный проспект, 60 к1, Питер') }
-      #let!(:track) { create(:track, car_id: car, order_id: order) }
-      let!(:status1) { create(:status_car,
+        let!(:order) { create(:order,
+                              take_time: '2016-10-09 12:00:00',
+                              end_time: '2016-10-09 13:00:00',
                               car_id: car.id,
-                              geo_lat: order.begin_lat,
-                              geo_lon: order.begin_lon,
-                              fixed_time: '2016-10-09 12:10:00') }
-      let!(:status2) { create(:status_car,
-                              car_id: car.id,
-                              geo_lat: 60.1255121,
-                              geo_lon: 30.3888101,
-                              fixed_time: '2016-10-09 12:30:00') }
-      let!(:status3) { create(:status_car,
-                              car_id: car.id,
-                              geo_lat: order.end_lat,
-                              geo_lon: order.end_lon,
-                              fixed_time: '2016-10-09 12:50:00') }
+                              begin_address: 'МО №17 "Шувалово-Озерки", Питер',
+                              end_address: 'Северный проспект, 60 к1, Питер') }
+        #let!(:track) { create(:track, car_id: car, order_id: order) }
+        let!(:status1) { create(:status_car,
+                                car_id: car.id,
+                                geo_lat: order.begin_lat,
+                                geo_lon: order.begin_lon,
+                                fixed_time: '2016-10-09 12:10:00') }
+        let!(:status2) { create(:status_car,
+                                car_id: car.id,
+                                geo_lat: 60.1255121,
+                                geo_lon: 30.3888101,
+                                fixed_time: '2016-10-09 12:30:00') }
+        let!(:status3) { create(:status_car,
+                                car_id: car.id,
+                                geo_lat: order.end_lat,
+                                geo_lon: order.end_lon,
+                                fixed_time: '2016-10-09 12:50:00') }
 
-      it 'expected return 4' do
-        car.define_tracks('2016-10-09 11:00:00','2016-10-09 14:00:00')
-        track = Track.all[0]
-        track.define_type
-        expect(track.track_type_id).to eq 4
+        it 'expected return 4' do
+          car.define_tracks('2016-10-09 11:00:00','2016-10-09 14:00:00')
+          track = Track.all[0]
+          track.define_type
+          expect(track.track_type_id).to eq 4
+        end
       end
+
+      context 'inspection-3 | Поездка по заказу' do
+        let!(:order) { create(:order,
+                              take_time: '2016-10-09 12:00:00',
+                              end_time: '2016-10-09 13:00:00',
+                              car_id: car.id,
+                              begin_address: 'МО №17 "Шувалово-Озерки", Питер',
+                              end_address: 'Северный проспект, 60 к1, Питер',
+                              distance: 13) }
+        let!(:status1) { create(:status_car,
+                                car_id: car.id,
+                                geo_lat: order.begin_lat,
+                                geo_lon: order.begin_lon,
+                                fixed_time: '2016-10-09 12:10:00') }
+        let!(:status2) { create(:status_car,
+                                car_id: car.id,
+                                geo_lat: order.end_lat,
+                                geo_lon: order.end_lon,
+                                fixed_time: '2016-10-09 12:50:00') }
+
+        it 'expected return 3' do
+          car.define_tracks('2016-10-09 11:00:00','2016-10-09 14:00:00')
+          track = Track.all[0]
+          track.define_type
+          expect(track.track_type_id).to eq 3
+        end
       end
     end
   end
