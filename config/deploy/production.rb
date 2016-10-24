c450e334027a080c19ab694f90416cfe4ab1933f
MaksimPW <ma.savichev@gmail.com>
2016-10-24 16:06:12 +0300

    master | deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '10.8.0.1', user: 'sa', roles: %w{app db web}, port: 2244, primary: true
+
+# server '10.8.0.1', user: 'sa', roles: %w{app db web}, port: 2244, primary: true
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
@@ -22,6 +23,7 @@ role :web, %w{sa@10.8.0.1}
 role :db, %w{sa@10.8.0.1}
 
 set :rails_env, :production
+set :stage, :production
 
 
 # Configuration
@@ -45,7 +47,8 @@ set :rails_env, :production
   set :ssh_options, {
     keys: %w(/home/sa/.ssh/id_rsa),
     forward_agent: true,
-    auth_methods: %w(publickey password)
+    auth_methods: %w(publickey password),
+    port: 2244
  }
 #
 # The server-based syntax can be used to override options:
