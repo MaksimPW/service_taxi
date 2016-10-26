1bd45da0d2d2e875e4c84edb21f9fbdb0d7118bf
MaksimPW <ma.savichev@gmail.com>
2016-10-27 02:46:08 +0300

    master | conver_car_number

--- app/models/waybill.rb
+++ app/models/waybill.rb
@@ -1,3 +1,10 @@
 class Waybill < ActiveRecord::Base
   self.primary_key = 'id'
+
+  after_create :convert_car_number
+
+  def convert_car_number
+    self.car_number = self.car_number.mb_chars.upcase.to_s.tr(' ','-')
+    self.save
+  end
 end
